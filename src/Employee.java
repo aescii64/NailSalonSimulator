c147acc1e8f9e8f88a713d6b0eb3f9f6aa96470f
Lynnece Eutsey <bharieutsey@outlook.com>
2026-01-06 21:12:14 -0600

    Employee Class

---
+++ src/Employee.java
@@ -0,0 +1,20 @@
+public class Employee {
+
+    private final String name;
+    private final int level;
+
+    public Employee(String name, int level) {
+        this.name = name;
+        this.level = level;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+}
+
+
