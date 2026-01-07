ebb4e97ee6e1586a499cd64ecfadc541fe933930
Lynnece Eutsey <bharieutsey@outlook.com>
2026-01-07 11:42:16 -0600

    using sets to determine if employees can perform certain services plus testing in main

--- src/Driver.java
+++ src/Driver.java
@@ -1,6 +1,24 @@
+import java.util.Set;
 
 public class Driver {
 
+    public static void main(String[] args) {
+        Employee Lvl1 = new Employee("Henry", Set.of(ServiceCategory.PEDICURE));
+        Employee Lvl2 = new Employee("Jessica", Set.of(ServiceCategory.MANICURE));
 
+        Service s1 = Service.GEL_MANICURE;
+        Service s2 = Service.BASIC_PEDICURE;
+
+        if (Lvl1.canPerform(Service.BASIC_PEDICURE)) {
+            System.out.println(Lvl1 + " will perform " + Service.BASIC_PEDICURE);
+        } else {
+            System.out.println(Lvl1 + " doesn't perform that service, choose another technician");
+        }
+        if (Lvl2.canPerform(Service.BASIC_PEDICURE)) {
+            System.out.println(Lvl2 + " will perform " + Service.BASIC_PEDICURE);
+        } else {
+            System.out.println(Lvl2 + " doesn't perform that service, choose another technician");
+        }
+    }
 
 }
--- src/Employee.java
+++ src/Employee.java
@@ -1,22 +1,29 @@
+import java.util.Set;
+
 public class Employee {
 
     private final String name;
+    private final Set<ServiceCategory> acceptedCategories;
 
-
-    public Employee(String name) {
+    public Employee(String name, Set<ServiceCategory> acceptedCategory) {
         this.name = name;
+        this.acceptedCategories = acceptedCategory;
     }
 
     public String getName() {
         return name;
     }
 
+    public Set<ServiceCategory> getAcceptedCategories() {
+        return acceptedCategories;
+    }
+
+    public boolean canPerform(Service service) {
+        return acceptedCategories.contains(service.getCategory());
+    }
+
     @Override
     public String toString() {
         return name;
     }
-
-
 }
-
-
