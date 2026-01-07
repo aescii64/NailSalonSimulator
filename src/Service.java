f9f354457d5751594aaf5e9fbde66a9ebdcc2137
Lynnece Eutsey <bharieutsey@outlook.com>
2026-01-06 21:06:56 -0600

    Service class

---
+++ src/Service.java
@@ -0,0 +1,20 @@
+public class Service {
+
+    private final String name;
+    private final Double price;
+
+    public Service(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
+
+
