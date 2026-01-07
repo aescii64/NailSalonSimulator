8361dc2c379a7bf19de69c9830f246eb7d4438df
Lynnece Eutsey <bharieutsey@outlook.com>
2026-01-07 12:12:34 -0600

    Customer class complete

--- src/Customer.java
+++ src/Customer.java
@@ -1,3 +1,23 @@
 public class Customer {
 
+    private final String name;
+    private final Service requestedService;
+
+    public Customer(String name, Service requestedService) {
+        this.name = name;
+        this.requestedService = requestedService;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Service getRequestedService() {
+        return requestedService;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
